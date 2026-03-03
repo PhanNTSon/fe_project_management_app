60ff2e0f10fed47479c34f8cad68f261825f148e
Phan Son <minikie456@gmail.com>
2026-03-03 15:13:24 +0700

    [Add] add routing path to pages and add Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/LandingPage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import UserDashboardPage from './pages/UserDashboardPage/UserDashboardPage'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import ProjectDetailPage from './pages/ProjectDetailPage/ProjectDetailPage'
 
-function AppContent(){
+function AppContent() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/dashboard" element={<UserDashboardPage />} />
+                <Route path="/project/:projectId" element={<ProjectDetailPage />} />
 
+                <Route path='*' element={<ErrorPage />} />
+            </Routes>
+        </Router>
+    )
 }
 
 function App() {
-  
+    return (
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    )
 }
 
 export default App
