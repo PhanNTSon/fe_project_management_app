6ff6e9d1326f9e33002baabcb685fb6a61417a7e
Phan Son <minikie456@gmail.com>
2026-03-19 12:13:19 +0700

    [Refactor] remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage';
 import ResetPasswordPage from './pages/ResetPasswordPage/ResetPasswordPage';
-import MembershipPlansPage from './pages/MembershipPlansPage/MembershipPlansPage';
-import JoinProjectViaInviteLinkPage from './pages/JoinProjectViaInviteLinkPage/JoinProjectViaInviteLinkPage';
 
 // Dashboard Pages
 import UserDashboard from './pages/UserDashboard/UserDashboard';
@@ -19,28 +17,6 @@ import ProjectDetailOverviewPage from './pages/ProjectDetailOverviewPage/Project
 import ProjectSettingsPage from './pages/ProjectSettingsPage/ProjectSettingsPage';
 import ProjectMembersPage from './pages/ProjectMembersPage/ProjectMembersPage';
 
-// Requirement / Document Pages
-import RequirementManagementPage from './pages/RequirementManagementPage/RequirementManagementPage';
-import SrsDocumentEditorPage from './pages/SrsDocumentEditorPage/SrsDocumentEditorPage';
-import DiagramEditorPage from './pages/DiagramEditorPage/DiagramEditorPage';
-import UseCaseBuilderPage from './pages/UseCaseBuilderPage/UseCaseBuilderPage';
-import ExportDocumentPage from './pages/ExportDocumentPage/ExportDocumentPage';
-import FileManagerPage from './pages/FileManagerPage/FileManagerPage';
-
-// Collaboration Pages
-import CommentDiscussionPage from './pages/CommentDiscussionPage/CommentDiscussionPage';
-import ReviewApprovalPage from './pages/ReviewApprovalPage/ReviewApprovalPage';
-import InviteMembersPage from './pages/InviteMembersPage/InviteMembersPage';
-import CompareVersionsPage from './pages/CompareVersionsPage/CompareVersionsPage';
-import ChangeHistoryPage from './pages/ChangeHistoryPage/ChangeHistoryPage';
-import ActivityLogPage from './pages/ActivityLogPage/ActivityLogPage';
-
-// User & Settings Pages
-import UserProfileSettingsPage from './pages/UserProfileSettingsPage/UserProfileSettingsPage';
-import NotificationsPage from './pages/NotificationsPage/NotificationsPage';
-import PaymentCheckoutPage from './pages/PaymentCheckoutPage/PaymentCheckoutPage';
-import PaymentHistoryPage from './pages/PaymentHistoryPage/PaymentHistoryPage';
-import CreatePremiumPlanPage from './pages/CreatePremiumPlanPage/CreatePremiumPlanPage';
 
 function App() {
   return (
@@ -49,8 +25,6 @@ function App() {
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<LandingPage />} />
-          <Route path="/pricing" element={<MembershipPlansPage />} />
-          <Route path="/join-project" element={<JoinProjectViaInviteLinkPage />} />
 
           {/* Guest-only Routes */}
           <Route path="/login" element={<GuestRoute><LoginPage /></GuestRoute>} />
@@ -66,31 +40,6 @@ function App() {
           <Route path="/projects/:projectId/settings" element={<ProtectedRoute><ProjectSettingsPage /></ProtectedRoute>} />
           <Route path="/projects/:projectId/members" element={<ProtectedRoute><ProjectMembersPage /></ProtectedRoute>} />
 
-          {/* Protected Requirement / Document Routes */}
-          <Route path="/projects/:projectId/requirements" element={<ProtectedRoute><RequirementManagementPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/srs-editor" element={<ProtectedRoute><SrsDocumentEditorPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/diagram-editor" element={<ProtectedRoute><DiagramEditorPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/use-case-builder" element={<ProtectedRoute><UseCaseBuilderPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/export" element={<ProtectedRoute><ExportDocumentPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/files" element={<ProtectedRoute><FileManagerPage /></ProtectedRoute>} />
-
-          {/* Protected Collaboration Routes */}
-          <Route path="/projects/:projectId/comments" element={<ProtectedRoute><CommentDiscussionPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/reviews" element={<ProtectedRoute><ReviewApprovalPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/invite" element={<ProtectedRoute><InviteMembersPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/compare" element={<ProtectedRoute><CompareVersionsPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/history" element={<ProtectedRoute><ChangeHistoryPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/changes" element={<ProtectedRoute><ChangeHistoryPage /></ProtectedRoute>} />
-          <Route path="/projects/:projectId/activity" element={<ProtectedRoute><ActivityLogPage /></ProtectedRoute>} />
-
-          {/* Protected User & Settings Routes */}
-          <Route path="/profile" element={<ProtectedRoute><UserProfileSettingsPage /></ProtectedRoute>} />
-          <Route path="/notifications" element={<ProtectedRoute><NotificationsPage /></ProtectedRoute>} />
-          <Route path="/checkout" element={<ProtectedRoute><PaymentCheckoutPage /></ProtectedRoute>} />
-          <Route path="/payment-history" element={<ProtectedRoute><PaymentHistoryPage /></ProtectedRoute>} />
-          
-          {/* Admin Routes */}
-          <Route path="/admin/plans/new" element={<ProtectedRoute><CreatePremiumPlanPage /></ProtectedRoute>} />
         </Routes>
       </Router>
     </AppProvider>
